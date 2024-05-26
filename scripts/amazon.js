d7f51220b11b63a2bb97af7eb8fc7f262d5c08a4
ZIYAD K <80669764+ziyadkadekara@users.noreply.github.com>
2024-05-26 06:57:39 +0530

    added productId

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -45,8 +45,8 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${
-            product.name
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${
+            product.id
           }">
             Add to Cart
           </button>
@@ -57,11 +57,11 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
-    const productName = button.dataset.productName;
+    const productId = button.dataset.productId;
 
     let matchingItem;
     cart.forEach((item) => {
-      if (productName === item.productName) {
+      if (productId === item.productId) {
         matchingItem = item;
       }
     });
@@ -69,7 +69,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       matchingItem.quantity += 1;
     } else {
       cart.push({
-        productName: productName,
+        productId: productId,
         quantity: 1,
       });
     }
