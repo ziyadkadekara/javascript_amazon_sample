6ebf8df92e2833e39a6ffcffcfce78e4e42d552a
ZIYAD K <80669764+ziyadkadekara@users.noreply.github.com>
2024-05-29 21:49:13 +0530

    added formatCurrency fucntion using money.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from '../data/cart.js';
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let cartSummaryHTML = '';
 
@@ -27,9 +28,9 @@ cart.forEach((cartItem) => {
                 <div class="product-name">
                   ${matchingProduct.name}
                 </div>
-                <div class="product-price">$${(
-                  matchingProduct.priceCents / 100
-                ).toFixed(2)}</div>
+                <div class="product-price">$${formatCurrency(
+                  matchingProduct.priceCents
+                )}</div>
                 <div class="product-quantity">
                   <span> Quantity: <span class="quantity-label">${
                     cartItem.quantity
