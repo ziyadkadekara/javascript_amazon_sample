7c707e8984de1b0cce0228e01db6a68f7f894c8d
ZIYAD K <80669764+ziyadkadekara@users.noreply.github.com>
2024-05-30 22:47:56 +0530

    maintaining the streak

--- data/cart.js
+++ data/cart.js
@@ -9,7 +9,7 @@ if (!cart) {
     { productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', quantity: 1 },
   ];
 }
-
+//ading a comment to maintain the streak
 //function to save to local storage
 function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
