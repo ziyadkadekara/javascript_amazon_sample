7c1912b00b7d1c2c8cc60236e990c112c9393cfe
ZIYAD K <80669764+ziyadkadekara@users.noreply.github.com>
2024-05-31 21:35:21 +0530

    export default note in money.js

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,8 @@
 export function formatCurrency(priceCents) {
   return (priceCents / 100).toFixed(2);
 }
+
+//export default formatCurrency;
+//each file can only have one default export
+//by this way we can use "import formatCurrency from blah blah"
+//so we can avoid curly brackss and make it clean, it depends on my mood ;)
