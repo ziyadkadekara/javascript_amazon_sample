30aac040162190f954bfe5ed1eaf9bacef2eb2c0
ZIYAD K <80669764+ziyadkadekara@users.noreply.github.com>
2024-05-31 22:11:00 +0530

    added deliveryoptions.js and new element to cart.js

--- data/cart.js
+++ data/cart.js
@@ -5,8 +5,13 @@ if (!cart) {
     {
       productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
       quantity: 2,
+      deliveryOptionId: '1',
     },
-    { productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', quantity: 1 },
+    {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2',
+    },
   ];
 }
 //ading a comment to maintain the streak
@@ -28,6 +33,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
+      deliveryOptionId: '1',
     });
   }
   saveToStorage();
