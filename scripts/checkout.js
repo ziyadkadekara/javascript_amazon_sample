a2fc719c5bbdea15f182640612da7d5b1909d009
ZIYAD K <80669764+ziyadkadekara@users.noreply.github.com>
2024-05-31 22:45:31 +0530

    added delivery optios html function

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -59,47 +59,14 @@ cart.forEach((cartItem) => {
                 <div class="delivery-options-title">
                   Choose a delivery option:
                 </div>
-                <div class="delivery-option">
-                  <input
-                    type="radio"
-                    checked
-                    class="delivery-option-input"
-                    name="delivery option-${matchingProduct.id}"
-                  />
-                  <div>
-                    <div class="delivery-option-date">Tuesday, June 21</div>
-                    <div class="delivery-option-price">FREE Shipping</div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input
-                    type="radio"
-                    class="delivery-option-input"
-                    name="delivery option-${matchingProduct.id}"
-                  />
-                  <div>
-                    <div class="delivery-option-date">Wednesday, June 15</div>
-                    <div class="delivery-option-price">$4.99 - Shipping</div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input
-                    type="radio"
-                    class="delivery-option-input"
-                    name="delivery option-${matchingProduct.id}"
-                  />
-                  <div>
-                    <div class="delivery-option-date">Monday, June 13</div>
-                    <div class="delivery-option-price">$9.99 - Shipping</div>
-                  </div>
-                </div>
+                ${deliveryOptionsHTML(matchingProduct, cartItem)}
               </div>
             </div>
           </div>
     `;
 });
 
-function deliveryOptionsHTML() {
+function deliveryOptionsHTML(matchingProduct, cartItem) {
   let html = '';
   deliveryOptions.forEach((deliveryOption) => {
     const today = dayjs();
@@ -110,15 +77,18 @@ function deliveryOptionsHTML() {
         ? 'FREE'
         : `$${formatCurrency(deliveryOption.priceCents)} -`;
 
+    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+
     html += `<div class="delivery-option">
     <input
       type="radio"
+       ${isChecked ? 'checked' : ''} 
       class="delivery-option-input"
       name="delivery option-${matchingProduct.id}"
     />
     <div>
       <div class="delivery-option-date">${dateString}</div>
-      <div class="delivery-option-price">${priceString} - Shipping</div>
+      <div class="delivery-option-price">${priceString} Shipping</div>
     </div>
   </div>`;
   });
