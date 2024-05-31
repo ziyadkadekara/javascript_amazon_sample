7df2576be6f5241b2aedca09df67d12c0d4e2288
ZIYAD K <80669764+ziyadkadekara@users.noreply.github.com>
2024-05-31 21:34:47 +0530

    added external libraries - dayjs added as modules

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,12 @@
 import { cart, removeFromCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.11/esm/index.js';
+
+// const today = dayjs();
+// const deliveryDate = today.add(7, 'days');
+
+// console.log(deliveryDate.format('dddd, MMMM D'));
 
 let cartSummaryHTML = '';
 
