74da3b095006417a2766b8580af95d3a1e8f493a
ZIYAD K <80669764+ziyadkadekara@users.noreply.github.com>
2024-05-31 22:45:06 +0530

    added function to generate html for delivery options

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@ import { cart, removeFromCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.11/esm/index.js';
+import { deliveryOptions } from '../data/deliveryOptions.js';
 
 // const today = dayjs();
 // const deliveryDate = today.add(7, 'days');
@@ -97,6 +98,32 @@ cart.forEach((cartItem) => {
           </div>
     `;
 });
+
+function deliveryOptionsHTML() {
+  let html = '';
+  deliveryOptions.forEach((deliveryOption) => {
+    const today = dayjs();
+    const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
+    const dateString = deliveryDate.format('dddd, MMMM D');
+    const priceString =
+      deliveryOption.priceCents === 0
+        ? 'FREE'
+        : `$${formatCurrency(deliveryOption.priceCents)} -`;
+
+    html += `<div class="delivery-option">
+    <input
+      type="radio"
+      class="delivery-option-input"
+      name="delivery option-${matchingProduct.id}"
+    />
+    <div>
+      <div class="delivery-option-date">${dateString}</div>
+      <div class="delivery-option-price">${priceString} - Shipping</div>
+    </div>
+  </div>`;
+  });
+  return html;
+}
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 
