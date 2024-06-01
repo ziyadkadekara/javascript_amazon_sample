58d6833ea3cd00f22a895e5e42d5c2b54e63e643
ZIYAD K <80669764+ziyadkadekara@users.noreply.github.com>
2024-06-01 23:06:57 +0530

    updated paymentSummary with totalcharges and deliverycharges

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -2,7 +2,10 @@ import { cart, removeFromCart, updateDeliveryOption } from '../../data/cart.js';
 import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.11/esm/index.js';
-import { deliveryOptions } from '../../data/deliveryOptions.js';
+import {
+  deliveryOptions,
+  getDeliveryOption,
+} from '../../data/deliveryOptions.js';
 
 // const today = dayjs();
 // const deliveryDate = today.add(7, 'days');
@@ -17,12 +20,7 @@ export function renderOrderSummary() {
     const matchingProduct = getProduct(productId);
 
     const deliveryOptionId = cartItem.deliveryOptionId;
-    let deliveryOption;
-    deliveryOptions.forEach((option) => {
-      if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-      }
-    });
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
 
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,3 +1,17 @@
+import { cart } from '../../data/cart.js';
+import { getProduct } from '../../data/products.js';
+import { getDeliveryOption } from '../../data/deliveryOptions.js';
+
 export function renderPaymentSummary() {
-  console.log('done');
+  let productPricecents = 0;
+  let shippingCharges = 0;
+
+  cart.forEach((cartItem) => {
+    const product = getProduct(cartItem.productId);
+    productPricecents += product.priceCents * cartItem.quantity;
+    const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
+    shippingCharges += deliveryOption.priceCents;
+  });
+  console.log(productPricecents);
+  console.log(shippingCharges);
 }
