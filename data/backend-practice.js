332f77c05f0c21cb3565701c6d7be8fca87dc847
ZIYAD K <80669764+ziyadkadekara@users.noreply.github.com>
2024-06-19 12:20:07 +0530

    learned http request

--- data/backend-practice.js
+++ data/backend-practice.js
@@ -4,5 +4,6 @@ xhr.addEventListener('load', () => {
   console.log(xhr.response);
 });
 
-xhr.open('GET', 'https://supersimplebackend.dev');
+// xhr.open('GET', 'https://supersimplebackend.dev');
+xhr.open('GET', 'https://supersimplebackend.dev/hello');
 xhr.send();
