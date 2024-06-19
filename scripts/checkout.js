6d4e6e5dae8f9b8660812668fc4bb7d3c70b544a
ZIYAD K <80669764+ziyadkadekara@users.noreply.github.com>
2024-06-19 22:47:17 +0530

    updated checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,12 @@
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
+import { loadProducts } from '../data/products.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
-renderOrderSummary();
-renderPaymentSummary();
-renderCheckoutHeader();
+loadProducts(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+  renderCheckoutHeader();
+});
