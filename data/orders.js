cf6162f30a3ecd149fd2bb3caaa7e5e427d02835
ZIYAD K <80669764+ziyadkadekara@users.noreply.github.com>
2024-06-21 00:15:39 +0530

    updated orders.js

--- data/orders.js
+++ data/orders.js
@@ -1,6 +1,6 @@
 export const orders = JSON.parse(localStorage.getItem('orders')) || [];
 
-function addOrder(order) {
+export function addOrder(order) {
   orders.unshift(order);
   saveToStorage();
 }
